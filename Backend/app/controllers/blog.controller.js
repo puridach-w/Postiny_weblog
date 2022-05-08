b46f45d5fc6840091566a87502330142a069172a
unknown <prariya63@gmail.com>
2022-05-08 10:42:26 +0700

    add comment

--- Backend/app/controllers/blog.controller.js
+++ Backend/app/controllers/blog.controller.js
@@ -74,8 +74,150 @@ const unlike = (req, res) => {
    }); 
 }
 
+const getAllComment = (req, res) => {
+    pool.getConnection((err, db) => {
+        if (err) {
+            console.log(err);
+            db.release();
+            return;
+        }
+        const article_id = req.params.article_id;
+        db.query(`SELECT c.comment_id, c.content, c.created_at, u.user_id, u.username
+        FROM comment c JOIN userinfo u ON c.user_id = u.user_id
+        WHERE c.article_id = ?
+        ORDER BY created_at DESC
+        `,
+        [article_id], (err, result) => {
+            if (err) {
+                console.log(err);
+            } else {
+                res.send(result);
+            }
+            db.release();
+        });
+   }); 
+}
+
+const addComment = (req, res) => {
+    pool.getConnection((err, db) => {
+        if (err) {
+            console.log(err);
+            db.release();
+            return;
+        }
+        const article_id = req.body.article_id;
+        const user_id = req.body.user_id;
+        const content = req.body.content;
+        db.query(`INSERT INTO comment (user_id, article_id, content)
+        VALUES (?, ?, ?)        
+        `,
+        [user_id, article_id, content], (err, result) => {
+            if (err) {
+                console.log(err);
+            } else {
+                res.send(result);
+            }
+            db.release();
+        });
+   }); 
+}
+
+const updateComment = (req, res) => {
+    pool.getConnection((err, db) => {
+        if (err) {
+            console.log(err);
+            db.release();
+            return;
+        }
+        const content = req.body.content;
+        const comment_id = req.body.comment_id;
+        db.query(`UPDATE comment
+        SET content = ?, updated_at = CURRENT_TIMESTAMP
+        WHERE comment_id = ?`,
+        [content, comment_id], (err, result) => {
+            if (err) {
+                console.log(err);
+            } else {
+                res.send(result);
+            }
+            db.release();
+        });
+   }); 
+}
+
+const deleteComment = (req, res) => {
+    pool.getConnection((err, db) => {
+        if (err) {
+            console.log(err);
+            db.release();
+            return;
+        }
+        const comment_id = req.query.comment_id;
+        db.query(`DELETE FROM comment
+        WHERE comment_id = ?`,
+        [comment_id], (err, result) => {
+            if (err) {
+                console.log(err);
+            } else {
+                res.send(result);
+            }
+            db.release();
+        });
+   }); 
+}
+
+const addViewing = (req, res) => {
+    pool.getConnection((err, db) => {
+        if (err) {
+            console.log(err);
+            db.release();
+            return;
+        }
+        const article_id = req.body.article_id;
+        const user_id = req.body.user_id;
+        db.query(`INSERT INTO articleviewing (user_id, article_id)
+        VALUES (?, ?)`,
+        [user_id, article_id], (err, result) => {
+            if (err) {
+                console.log(err);
+            } else {
+                res.send(result);
+            }
+            db.release();
+        });
+   }); 
+}
+
+const getViewing = (req, res) => {
+    pool.getConnection((err, db) => {
+        if (err) {
+            console.log(err);
+            db.release();
+            return;
+        }
+        const article_id = req.params.article_id;
+        const user_id = req.params.user_id;
+        db.query(`SELECT * From articleviewing 
+        WHERE article_id=? AND user_id=?`,
+        [article_id,user_id], (err, result) => {
+            if (err) {
+                console.log(err);
+            } else {
+                res.send(result);
+            }
+            db.release();
+        });
+   }); 
+}
+
 module.exports = {
     isLiked,
     like,
-    unlike
+    unlike,
+    getAllComment,
+    addComment,
+    updateComment,
+    deleteComment,
+    addViewing,
+    getViewing
 }
--- Backend/app/routes/blog.routes.js
+++ Backend/app/routes/blog.routes.js
@@ -2,6 +2,16 @@ const controller = require("../controllers/blog.controller");
 
 module.exports = function (app) {
     app.get("/isLiked", controller.isLiked);
+    app.get("/getAllComment/:article_id", controller.getAllComment)
+    app.get("/getViewing/:article_id/:user_id", controller.getViewing)
+
     app.post("/like", controller.like);
+    app.post("/addComment", controller.addComment)
+    app.post("/addviewing", controller.addViewing)
+
+    app.patch("/updateComment", controller.updateComment);
+
     app.delete("/unlike", controller.unlike);
+    app.delete("/deleteComment", controller.deleteComment);
+
 }
